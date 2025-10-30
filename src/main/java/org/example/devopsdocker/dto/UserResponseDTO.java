64dd6f48ea24925738a0613b6f5915f2a01d50db
efraimmf <emfs6@aluno.ifal.edu.br>
2025-10-30 16:51:31 -0300

    fix: add annotation for constructor without arguments

--- src/main/java/org/example/devopsdocker/dto/UserResponseDTO.java
+++ src/main/java/org/example/devopsdocker/dto/UserResponseDTO.java
@@ -2,11 +2,13 @@ package org.example.devopsdocker.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserResponseDTO {
     private Long id;
     private String username;
