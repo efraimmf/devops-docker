b1e948539e2839c9428edc2ec028740bcb2fb2dd
efraimmf <emfs6@aluno.ifal.edu.br>
2025-10-30 16:03:42 -0300

    refactor: adjust import from User Response DTO

--- src/main/java/org/example/devopsdocker/dto/UserResponseDTO.java
+++ src/main/java/org/example/devopsdocker/dto/UserResponseDTO.java
@@ -2,13 +2,11 @@ package org.example.devopsdocker.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class UserResponseDTO {
     private Long id;
     private String username;
