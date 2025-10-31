a690f0e3f1d74a40e748b75babfee227cdd3449d
efraimmf <emfs6@aluno.ifal.edu.br>
2025-10-31 00:16:49 -0300

    feat: add findAll by order

--- src/main/java/org/example/devopsdocker/repository/UserRepository.java
+++ src/main/java/org/example/devopsdocker/repository/UserRepository.java
@@ -3,9 +3,11 @@ package org.example.devopsdocker.repository;
 import org.example.devopsdocker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+    List<User> findAllByOrderByIdAsc();
 }
