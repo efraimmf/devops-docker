68fc8591405bbba563de0c16b8a4d03ea060a445
efraimmf <emfs6@aluno.ifal.edu.br>
2025-11-28 01:41:02 -0300

    test: remove MockitoExtension from UserControllerTest

--- src/test/java/org/example/devopsdocker/controller/UserControllerTest.java
+++ src/test/java/org/example/devopsdocker/controller/UserControllerTest.java
@@ -23,7 +23,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
-@ExtendWith(MockitoExtension.class)
 class UserControllerTest {
 
     @Autowired
