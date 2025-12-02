3561e25c1dd27d0cbc5e451b7a5d20102f8d5c53
efraimmf <emfs6@aluno.ifal.edu.br>
2025-12-02 19:17:11 -0300

    test: remove unused imports from UserControllerTest

--- src/test/java/org/example/devopsdocker/controller/UserControllerTest.java
+++ src/test/java/org/example/devopsdocker/controller/UserControllerTest.java
@@ -5,8 +5,6 @@ import org.example.devopsdocker.dto.UpdateUserDTO;
 import org.example.devopsdocker.dto.UserResponseDTO;
 import org.example.devopsdocker.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
